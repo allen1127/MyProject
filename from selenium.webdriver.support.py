36c275abdd83b10ca08830873205a9282a647e47
allen1127 <42729782+allen1127@users.noreply.github.com>
2025-01-02 10:02:02 +0800

    Update from selenium.webdriver.support.py

--- from selenium.webdriver.support.py
+++ from selenium.webdriver.support.py
@@ -8,8 +8,9 @@ import os
 import time
 from seleniumbase import Driver
 sites = ["John", "Mary", "Peter"];
-bookingHour = 9;
-slot = "3";
+bookingHour1 = 9;
+bookingHour2 = 15;
+slot = "1";
 
 
 driver = Driver(uc=True, headless=False)
@@ -26,10 +27,9 @@ time.sleep(2)
 def timer():
     print("1")
     datetimeNow = datetime.now() 
-    if "00"==datetimeNow.strftime('%H'):  
-        t.cancel()  
-        booking("3",9)
-        booking("3",15)
+    if "09"==datetimeNow.strftime('%H'):   
+        booking(slot,bookingHour1)
+        booking(slot,bookingHour2)
     else:
         t = Timer(0.5, timer)
         t.start()
@@ -37,16 +37,19 @@ t = Timer(0.5, timer)
 t.start()
 
 
-def booking(slot,bookingHour):
+def booking(rSlot,bookingHour):
     bookingday = datetime.now() + timedelta(days=13)
-    driver.get("https://scr.cyc.org.tw/tp10.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D="+bookingday.strftime('%Y/%m/%d')+"&D2="+slot)
+    driver.get("https://scr.cyc.org.tw/tp10.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D="+bookingday.strftime('%Y/%m/%d')+"&D2="+rSlot)
     timeout = 30
     WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"ContentPlaceHolder1_Date_Step2_lab\"]/select")))
     
-    for i in range(bookingHour, bookingHour+5):
+    for i in range(bookingHour, bookingHour+1):
         l =driver.find_element(by=By.XPATH, value="//*[@id=\"ContentPlaceHolder1_Step2_data\"]/table/tbody/tr["+str(i)+"]/td[3]/img")
         if l.get_attribute('src') == "https://scr.cyc.org.tw/img/place01.png":
-            # driver.execute_script("window.open('https://www.example.com')")
+            # print(i)
+            # driver.execute_script("window.open('https://www.google.com.tw/?hl=zh_TW')")
+            
+
             l.click()
             try:
                 WebDriverWait(driver, 3).until(EC.alert_is_present(),
@@ -65,7 +68,10 @@ def booking(slot,bookingHour):
             print(result.text)
             
             if result.text == "預約成功":
+                print("預約成功")
                 break
+
+
 
 print("hour")
 os.system("pause")
